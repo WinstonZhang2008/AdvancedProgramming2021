e9d7803c41b46c145a119399330322bf59e11a44
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2021-11-23 00:48:15 -0800

    Added some properties and actions to the Ultrasonic Subsystem

--- src/main/java/frc/robot/subsystems/ultrasonicSensor/UltrasonicDistanceSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ultrasonicSensor/UltrasonicDistanceSensorSubsystem.java
@@ -4,11 +4,22 @@
 
 package frc.robot.subsystems.ultrasonicSensor;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class UltrasonicDistanceSensorSubsystem extends SubsystemBase {
+
+  protected PIDSubsystem pidSub;
+  private Ultrasonic.Unit units;
   /** Creates a new ExampleSubsystem. */
-  public UltrasonicDistanceSensorSubsystem() {}
+  public UltrasonicDistanceSensorSubsystem(int pingChannel, int echoChannel) {}
+  public UltrasonicDistanceSensorSubsystem(int pingChannel, int echoChannel, Ultrasonic.Unit units) {}
+  public UltrasonicDistanceSensorSubsystem(DigitalOutput pingChannel, DigitalInput echoChannel) {}
+  public UltrasonicDistanceSensorSubsystem(DigitalOutput pingChannel, DigitalInput echoChannel, Ultrasonic.Unit units) {}
 
   @Override
   public void periodic() {
@@ -19,4 +30,32 @@ public class UltrasonicDistanceSensorSubsystem extends SubsystemBase {
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+
+  public void close() {
+
+  }
+
+  public Ultrasonic.Unit getDistanceUnits() {
+    return units;
+  }
+
+  
+  public double	getRangeInches() {
+    return 0;}	//Get the range in inches from the ultrasonic sensor.
+  public double	getRangeMM() {
+    return 0;}	//Get the range in millimeters from the ultrasonic sensor.
+  public void	initSendable​(SendableBuilder builder) {}	//Initializes this Sendable object.
+  public boolean	isEnabled() {
+    return false;}	//Is the ultrasonic enabled.
+  public boolean	isRangeValid() {
+    return false;}	//Check if there is a valid range measurement.
+  public double	pidGet() {
+    return 0;}	//Get the range in the current DistanceUnit for the PIDSource base object.
+  public void	ping() {}	//Single ping to ultrasonic sensor.
+  public static void	setAutomaticMode​(boolean enabling) {}	//Turn Automatic mode on/off for all sensors.
+  public void	setDistanceUnits​(Ultrasonic.Unit units) {}	//Set the current DistanceUnit that should be used for the PIDSource base object.
+  public void	setEnabled​(boolean enable) {}	//Set if the ultrasonic is enabled.
+  public void	setPIDSourceType​(PIDSubsystem pidSource) {}
+  
 }
+
