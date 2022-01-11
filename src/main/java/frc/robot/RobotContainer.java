6e8cf1983c73e5dce49b1f6dab75c2ab88c78d4f
Karan Saini <karansaini117@gmail.com>
2022-01-10 20:40:15 -0800

    cleaned up master

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ElevatorUp;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.PaddedXbox;
 import edu.wpi.first.wpilibj2.command.Command;
 
