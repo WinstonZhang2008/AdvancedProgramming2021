ee066772f325749f01896a0a2b867683615eef18
Franklin <>
2022-01-12 16:31:11 -0800

    telebop

--- src/main/java/frc/robot/subsystems/limitSwitch/limitSwitch.java
+++ src/main/java/frc/robot/subsystems/limitSwitch/limitSwitch.java
@@ -1,50 +1,50 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+  // Copyright (c) FIRST and other WPILib contributors.
+  // Open Source Software; you can modify and/or share it under the terms of
+  // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.limitswitch;
+  package frc.robot.subsystems.limitswitch;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+  import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.drivebase.DriveBaseSub;
+  import edu.wpi.first.wpilibj2.command.CommandBase;
+  import frc.robot.drivebase.DriveBaseSub;
 
 
-//telebop
-//telebop
+  //telebop
+  //telebop
 
-public class limitswitch extends CommandBase {
+  public class limitswitch extends CommandBase {
 
-  private double power;
-  /** Creates a new limitSwitch. */
-  public limitswitch(double power) {
-    this.power = power;
+    private double power;
+    /** Creates a new limitSwitch. */
+    public limitswitch(double power) {
+      this.power = power;
+      
+      // Use addRequirements() here to declare subsystem dependencies.
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
     
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+      //this is the power they told me to use
+      if (power > 0.4) {
+        DriveBaseSub.setPower(0.4);
+      } else {
+        DriveBaseSub.setPower(power);
+      }
+    }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-  
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    //this is the power they told me to use
-    if (power > 0.4) {
-      DriveBaseSub.setPower(0.4);
-    } else {
-      DriveBaseSub.setPower(power);
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return false;
     }
   }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
-}
