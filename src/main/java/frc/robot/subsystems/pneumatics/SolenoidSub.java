fb6348b2e57adba7c48014b209672d4d52d79700
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-01-13 23:35:02 -0800

    I coded the Autonomous program for the Pneumatics System. Will add/modify more if needed and be ready to test soon.

--- src/main/java/frc/robot/subsystems/pneumatics/SolenoidSub.java
+++ src/main/java/frc/robot/subsystems/pneumatics/SolenoidSub.java
@@ -3,17 +3,21 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.pneumatics;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SolenoidSub extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  Solenoid solenoid;
+  DoubleSolenoid solenoid;
+  //boolean reversed;
+  
 
   public SolenoidSub() {
-    solenoid = new Solenoid(1);
-    solenoid.set(true);
+    solenoid = new DoubleSolenoid(1,0,1);
+    //this.reversed = reversed;
   }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
@@ -24,4 +28,15 @@ public class SolenoidSub extends SubsystemBase {
     // This method will be called once per scheduler run during simulation
   }
 
+  public void actuateSolenoid() {
+    solenoid.set(Value.kForward);
+  }
+
+  public void retractSolenoid() {
+    solenoid.set(Value.kReverse);
+  }
+
+  public void stopSolenoid() {
+    solenoid.set(Value.kOff);
+  }
 }
