b62c1761d1fb052b2a5d78aaf22a0c97b4c5baac
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-01-15 14:00:16 -0800

    Made some modifications

---
+++ src/main/java/frc/robot/subsystems/autos/DriveForwardIntakeShoot.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class DriveForwardIntakeShoot extends SequentialCommandGroup{
+    public DriveForwardIntakeShoot() {
+        addCommands();
+    }
+}
