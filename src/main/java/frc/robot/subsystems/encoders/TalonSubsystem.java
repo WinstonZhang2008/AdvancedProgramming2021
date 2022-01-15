e144be4d426e800857fff018b8b7e249fec7013e
jinnie20 <ktakwu@gmail.com>
2022-01-14 16:37:06 -0800

    TalonFX code

--- src/main/java/frc/robot/subsystems/encoders/TalonSubsystem.java
+++ src/main/java/frc/robot/subsystems/encoders/TalonSubsystem.java
@@ -6,9 +6,13 @@ package frc.robot.subsystems.encoders;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+  private TalonFX talon;
+
 public class TalonSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public TalonSubsystem() {}
+  public TalonSubsystem() {
+      talon = new TalonFX(0);
+  }
 
   @Override
   public void periodic() {
@@ -18,5 +22,9 @@ public class TalonSubsystem extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  }
+
+  public getTalon() {
+    return talon;
   }
 }
